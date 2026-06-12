18baa53d5d8772894f849723213fd588d9db93fc
rbjardim <ruan.bueno70@gmail.com>
2026-06-11 21:29:42 -0300

    adicionando politica de bot e termos

---
+++ CampoLimpoRP/CampoLimpoRP/Pages/Termos.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class TermosModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
--- CampoLimpoRP/CampoLimpoRP/obj/Debug/net10.0/CampoLimpoRP.AssemblyInfo.cs
+++ CampoLimpoRP/CampoLimpoRP/obj/Debug/net10.0/CampoLimpoRP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampoLimpoRP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12eeddef7815108c02577285d436082d093b07df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a83d95b504aa14ec0970979480ad27546521016")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampoLimpoRP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampoLimpoRP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
